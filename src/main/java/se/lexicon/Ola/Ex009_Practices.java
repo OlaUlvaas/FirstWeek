6cc4461a603714b65e8ddff7f9efc765e3958180
Ola Ulvaas <ola.ulvaas@gmail.com>
2020-12-08 20:34:11 +0100

    Practices

---
+++ src/main/java/se/lexicon/Ola/Ex009_Practices.java
@@ -0,0 +1,78 @@
+package se.lexicon.Ola;
+
+/**
+ * Practices:
+ * Create a String with your first name as content. Print out the String object to the console.
+ * Create two Strings. One with your first name and the other with your last name.
+ * Print out both Strings to the console with a ” ” separating them.
+ * Create two variables with decimal values. Create a third decimal variable called result.
+ * Assign the sum of the two first variables to result.
+ *
+ * Practices:
+ * Create a class called Car. The class should contain the following information.
+ * Car brand, registration number, max speed, owner name and one more attribute of your choice.
+ * Instantiate an Object of the class and assign values to the object.
+ * Make an integer called score and assign a value to it.
+ * Create an if-statement that should print out”Hurray, You passed!” if the score is above or equals to 65.
+ * If the score is Equal or between 55 and 64 it should print ”You are almost there.”.
+ * If score is below 55 it should print ”Sorry, You did not pass.”
+ */
+class Car{
+    String brand;
+    String regNumber;
+    int maxSpeed;
+    String owner;
+    int year;
+
+
+
+}
+public class Ex009_Practices {
+    public static void main(String[] args){
+        /*String firstName = "Ola";
+        System.out.println(firstName);*/
+
+        String firstName = "Ola";
+        String lastName = "Ulvås";
+        System.out.println(firstName + " " + lastName);
+
+       /* double num1 = 13.45;
+        double num2 = 2.224;
+        double result = num1 + num2;*/
+
+        /*Car car = new Car();
+        car.brand = "Opel Manta";
+        car.maxSpeed = 220;
+        car.regNumber = "FIN777";
+        car.owner = "Ola Ulvås";
+        car.year = 1974;
+
+        String brand = car.brand;
+        int maxSpeed = car.maxSpeed;
+        String regNumber = car.regNumber;
+        String owner = car.owner;
+        int year = car.year;
+
+        System.out.println(brand);
+        System.out.println(maxSpeed);
+        System.out.println(regNumber);
+        System.out.println(owner);
+        System.out.println(year);*/
+
+        /*int score = 65;
+
+        if(score == 65){
+            System.out.println("Hurray, You passed!");
+        }
+        else if(score >= 55 && score <= 64){
+            System.out.println("You are almost there!");
+        }
+        else if(score < 55){
+            System.out.println("Sorry, You did not pass.");
+        }*/
+
+
+
+
+    }
+}
